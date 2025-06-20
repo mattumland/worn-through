c407322bae72ea3e3a462e16b2dbd87bbd34ae76
Matt Umland <mattumland@gmail.com>
2025-06-20 11:04:12 -0400

    Add responsive image for mobile

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import GhostParallax from "./components/parallax";
 import Image from "next/image";
 import WTImage from "../public/cropped-sky.webp"
+import bigSky from "../public/big-sky.webp"
 import logo from "../public/record_logo.webp"
 import VideoSection from "./components/videoSection";
 import MusicSection from "./components/musicSection";
@@ -12,7 +13,7 @@ export default function Home() {
         <h1 className="hidden">Worn Through</h1>
         <div className="-z-10 fixed w-full">
           <div className="gap-0 grid grid-cols-1 grid-rows-1">
-            <div className="relative col-span-1 col-start-1 row-span-1 row-start-1 md:h-screen min-h-[45vh]">
+            <div className="relative col-span-1 col-start-1 row-span-1 row-start-1 h-screen">
               <Image
                 src={WTImage}
                 alt="Matt, Greg, Sophie, Matt and Nick of Worn Through standing in front of beautiful yellow forsythias with a huge blue sky in the background."
@@ -20,7 +21,16 @@ export default function Home() {
                 placeholder='blur'
                 fill={true}
                 style={{ objectFit: "cover" }}
-                className="-z-1"
+                className="invisible md:visible -z-1 md:"
+              />
+              <Image
+                src={bigSky}
+                alt="Matt, Greg, Sophie, Matt and Nick of Worn Through standing in front of beautiful yellow forsythias with a huge blue sky in the background."
+                quality={100}
+                placeholder='blur'
+                fill={true}
+                style={{ objectFit: "cover" }}
+                className="md:hidden -z-2"
               />
               <Image
                 src={logo}
@@ -31,7 +41,7 @@ export default function Home() {
           </div>
         </div>
 
-      <div className="md:h-screen min-h-[45vh]"></div>
+      <div className="h-screen"></div>
       <GhostParallax />
       <MusicSection />
       <VideoSection />
